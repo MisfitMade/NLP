c324bbec2de9697f79589085ee97d6a4de502a3b
michael <misfitmade2@gmail.com>
2022-10-01 10:56:17 -0600

    Fix typo

--- NLP_Lib.py
+++ NLP_Lib.py
@@ -7,7 +7,7 @@ PATH_TO_PLOTS = f"{PROJECT_ROOT_DIR}/plots"
 def save_fig(plt: matplotlib.pyplot, fig_id: str, tight_layout=True, fig_extension="png", resolution=300) -> None:
     '''
     Given a plot object and save specs/info, saves the plt object's current fig
-    to the global define plots folder path
+    to the globally defined plots folder path
     '''
     os.makedirs(PATH_TO_PLOTS, exist_ok=True)
     path = os.path.join(PATH_TO_PLOTS, fig_id + "." + fig_extension)
